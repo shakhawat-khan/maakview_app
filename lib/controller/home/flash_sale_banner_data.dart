13be3de4879b8e535b2032442fd11307b4723fa6
shakhawat-khan <satanfromhelll666@gmail.com>
2022-08-03 18:57:39 +0600

    'laptop'

--- lib/controller/home/flash_sale_banner_data.dart
+++ lib/controller/home/flash_sale_banner_data.dart
@@ -1,17 +1,18 @@
 import 'dart:convert';
+import 'dart:js';
 
 import 'package:flutter/material.dart';
 
-import '../../model/home/most_popular_banner_model.dart';
+import '../../../model/home/most_popular_banner_model.dart';
 
 import 'package:http/http.dart' as http ;
 
-class MostPopularBannerData extends StatelessWidget {
+class FlashSaleBannerData extends StatelessWidget {
 
 
   Future<MostPopularBannerModel> getBanner() async{
     http.Response response;
-    response = await http.get(Uri.parse('https://www.maakview.com/api/v1/setting/home/banner_section_one'));
+    response = await http.get(Uri.parse('https://www.maakview.com/api/v1/setting/home/banner_section_two'));
 
     if(response.statusCode == 200){
       return MostPopularBannerModel.fromJson(jsonDecode(response.body));
@@ -33,7 +34,7 @@ class MostPopularBannerData extends StatelessWidget {
           final bannerModel = snapshot.data;
 
           if(snapshot.hasData){
-            return buildSlider(bannerModel!);
+            return buildSlider(bannerModel!,context);
           } else if(snapshot.hasError){
             return Text('${snapshot.error}');
           }
@@ -45,13 +46,13 @@ class MostPopularBannerData extends StatelessWidget {
 
   }
 
-  buildSlider(MostPopularBannerModel mostPopularBannerModel){
+  buildSlider(MostPopularBannerModel mostPopularBannerModel,BuildContext context){
     final data = mostPopularBannerModel.data[0].img;
     return InkWell(
       onTap: (){},
       child: Container(
         height: 100,
-        width: double.infinity,
+        width: MediaQuery.of(context).size.width,
         child: Image.network(data,fit: BoxFit.fill,),
       ),
     );
